9ee2714393bf2f60a282f764034e20f5b22c9568
Cristian Gustavo Gomez <83784314+Cristian-Gomez-coder@users.noreply.github.com>
2024-01-31 14:51:12 -0300

    primer commit

---
+++ SistemaVentas/Controllers/SaleDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVentas.Controllers
+{
+    public class SaleDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaVentas/Models/SaleDetail.cs
@@ -0,0 +1,6 @@
+﻿namespace SistemaVentas.Models
+{
+    public class SaleDetail
+    {
+    }
+}
